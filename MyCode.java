beab620c655432994e30ebf6435eb6c97d095ac0
VivekRajuD <vivekdasaraju.java@gmail.com>
2025-07-01 18:20:10 +0530

    committed 2 commit

--- MyCode.java
+++ MyCode.java
@@ -3,7 +3,7 @@ public class MyCode {
         int num1 = 5;
         int num2 = 7;
         int result = multiply(num1, num2);
-        System.out.println(num1 + " * " + num2 + " = " + result+" Data");
+        System.out.println(num1 + " * " + num2 + " = " + result);
     }
 
     public static int multiply(int a, int b) {
