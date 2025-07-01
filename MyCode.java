89ffbdd83c37945d15e14d5d1c2d54b8313da38e
VivekRajuD <vivekdasaraju.java@gmail.com>
2025-07-01 18:02:37 +0530

    First commit

---
+++ MyCode.java
@@ -0,0 +1,12 @@
+public class MyCode {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int result = multiply(num1, num2);
+        System.out.println(num1 + " * " + num2 + " = " + result);
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
